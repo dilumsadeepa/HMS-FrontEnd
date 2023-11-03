012db8e2df25c8ca58fb8de9f36efd66e2657c17
Firstname Lastname <101855974+Avishkasj@users.noreply.github.com>
2023-11-03 10:07:57 +0530

    add assts

--- hostel_management_system/src/components/Admin/AddAsset.jsx
+++ hostel_management_system/src/components/Admin/AddAsset.jsx
@@ -3,14 +3,17 @@ import AdminNavbar from "./AdminNavbar";
 import AdminTopBar from "./AdminTopBar";
 import AdminFooter from "./AdminFooter";
 import DashboardTables from "./DashboardTables";
+import axios from "axios";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 const AddAsset = () => {
   const [asset, setAsset] = useState({
     roomno: "",
-    intdate: "",
-    name: "",
-    status: "",
-    // Add more fields as needed
+    intdate: null, // Use null for the date value
+    lastMaintDate: null, // Use null for the date value
+    name: "chair",
+    status: "active",
   });
 
   const handleInputChange = (e) => {
@@ -21,18 +24,42 @@ const AddAsset = () => {
     });
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Handle form submission here, e.g., make an API request to add the asset
-    console.log("Form submitted with data:", asset);
-    // Reset the form fields if needed
+  const handleDateChange = (name, date) => {
     setAsset({
-      roomno: "",
-      intdate: "",
-      name: "",
-      status: "",
-      // Reset other fields
+      ...asset,
+      [name]: date,
     });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const data = {
+      roomNo: asset.roomno,
+      resName: asset.name,
+      installationDate: asset.intdate,
+      lastMaintenanceDate: asset.lastMaintDate,
+      status: asset.status,
+    };
+
+    try {
+      const response = await axios.post(
+        "http://52.201.92.58:8080/res/add",
+        data
+      );
+
+      console.log("API Response:", response.data);
+
+      setAsset({
+        roomno: "",
+        intdate: null,
+        lastMaintDate: null,
+        name: "chair",
+        status: "active",
+      });
+    } catch (error) {
+      console.error("API Request Error:", error);
+    }
   };
 
   return (
@@ -43,102 +70,100 @@ const AddAsset = () => {
           <div id="content">
             <AdminTopBar />
             <div className="nclass mx-5">
-            <div className="container-fluid  ">
-              <h2>Add New Asset</h2>
-              <form onSubmit={handleSubmit}>
-                <div className="row">
-                  <div className="col-md-6">
-                    <div className="mb-3">
-                      <label htmlFor="roomno" className="form-label">
-                        Room No
-                      </label>
-                      <input
-                        type="number"
-                        className="form-control"
-                        id="roomno"
-                        name="roomno"
-                        value={asset.roomno}
-                        onChange={handleInputChange}
-                        required
-                      />
+              <div className="container-fluid  ">
+                <h2>Add New Asset</h2>
+                <form onSubmit={handleSubmit}>
+                  <div className="row">
+                    <div className="col-md-6">
+                      <div className="mb-3">
+                        <label htmlFor="roomno" className="form-label">
+                          Room No
+                        </label>
+                        <input
+                          type="number"
+                          className="form-control"
+                          id="roomno"
+                          name="roomno"
+                          value={asset.roomno}
+                          onChange={handleInputChange}
+                          required
+                        />
+                      </div>
+                    </div>
+                    <div className="col-md-3">
+                      <div className="mb-3">
+                        <label htmlFor="intdate" className="form-label">
+                          Installation Date
+                        </label>
+                        <DatePicker
+                          className="form-control"
+                          selected={asset.intdate}
+                          onChange={(date) => handleDateChange("intdate", date)}
+                          dateFormat="yyyy-MM-dd"
+                          required
+                        />
+                      </div>
+                    </div>
+                    <div className="col-md-3">
+                      <div className="mb-3">
+                        <label htmlFor="lastMaintDate" className="form-label">
+                          Last Maintenance Date
+                        </label>
+                        <DatePicker
+                          className="form-control"
+                          selected={asset.lastMaintDate}
+                          onChange={(date) =>
+                            handleDateChange("lastMaintDate", date)
+                          }
+                          dateFormat="yyyy-MM-dd"
+                          required
+                        />
+                      </div>
                     </div>
                   </div>
-                  <div className="col-md-6">
-                    <div className="mb-3">
-                      <label htmlFor="intdate" className="form-label">
-                        Installation Date
-                      </label>
-                      <input
-                        type="text"
-                        className="form-control"
-                        id="intdate"
-                        name="intdate"
-                        value={asset.intdate}
-                        onChange={handleInputChange}
-                        required
-                      />
+                  <div className="row">
+                    <div className="col-md-6">
+                      <div className="mb-3">
+                        <label htmlFor="name" className="form-label">
+                          Asset Name
+                        </label>
+                        <select
+                          className="form-control"
+                          id="name"
+                          name="name"
+                          value={asset.name}
+                          onChange={handleInputChange}
+                        >
+                          <option value="chair">Chair</option>
+                          <option value="desk">Desk</option>
+                          <option value="lamp">Lamp</option>
+                          <option value="fan">Fan</option>
+                          <option value="cubard">Cubard</option>
+                        </select>
+                      </div>
+                    </div>
+                    <div className="col-md-6">
+                      <div className="mb-3">
+                        <label htmlFor="status" className="form-label">
+                          Status
+                        </label>
+                        <select
+                          className="form-control"
+                          id="status"
+                          name="status"
+                          value={asset.status}
+                          onChange={handleInputChange}
+                        >
+                          <option value="active">Active</option>
+                          <option value="inactive">Inactive</option>
+                        </select>
+                      </div>
                     </div>
                   </div>
-                </div>
-                <div className="row">
-                  <div className="col-md-6">
-                    <div className="mb-3">
-                      <label htmlFor="name" className="form-label">
-                        Asset Name
-                      </label>
-                      <input
-                        type="text"
-                        className="form-control"
-                        id="name"
-                        name="name"
-                        value={asset.name}
-                        onChange={handleInputChange}
-                        required
-                      />
-                    </div>
-                  </div>
-                  <div className="col-md-6">
-                    <div className="mb-3">
-                      <label htmlFor="status" className="form-label">
-                        Status
-                      </label>
-                      <input
-                        type="text"
-                        className="form-control"
-                        id="status"
-                        name="status"
-                        value={asset.status}
-                        onChange={handleInputChange}
-                        required
-                      />
-                    </div>
-                  </div>
-                </div>
-
-                <div className="row">
-                  <div className="col-md-6">
-                    <div className="mb-3">
-                      <label htmlFor="name" className="form-label">
-                       Last Maintanance Date
-                      </label>
-                      <input
-                        type="text"
-                        className="form-control"
-                        id="name"
-                        name="name"
-                        value={asset.name}
-                        onChange={handleInputChange}
-                        required
-                      />
-                    </div>
-                  </div>
-                  
-                </div>
-                {/* Add more form fields for other asset details */}
-                <button type="submit" className="btn btn-primary">
-                  Add Asset
-                </button>
-              </form>
+                  <button type="submit" className="btn btn-primary">
+                    Add Asset
+                  </button>
+                </form>
               </div>
             </div>
           </div>
